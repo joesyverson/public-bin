377da35c516308342a52621ce26262b6c4ba815c
joes <josephgavinsyverson@gmail.com>
2019-11-28 23:43:16 -0500

    01: refactor and reformat output

--- 01-hello-bash.sh
+++ 01-hello-bash.sh
@@ -2,65 +2,75 @@
 
 # my first script
 	# for syntax study
-
 # this is gonna be a very long script
-
 # call with
-	# ./01.sh <foo> <bar> <baz>
+	# ./01-hello-bash.sh <foo> <bar> <baz>
 	# or
 	# bash 01-hello-bash.sh <foo> <bar> <baz>
+
+#################### BEGIN SCRIPT ####################
+
+echo " "
+echo "*************** ${0} ***************"
+
+counter=0
 
 # touch 01-out.txt
 # file=01-out.txt
 
+if [[ $# != 3 ]]; then
+	echo " "
+	echo "!error!"
+	echo " "
+	echo "wrong number of arguments. call with three arguments:"
+	echo "     ./${0}  <foo> <bar> <baz>"
+	echo " "
+	exit
+fi
+
+#################### FUNCTIONS ####################
+
 function format() {
+	# file=01-out.txt
 	echo " " # >> ${file}
-	echo ${1} # >> ${file}
-	echo " " # >> ${file}
+	let counter=$((counter+1))
+	echo "--------------- ${counter} ---------------"
+	echo " "
+	echo ${1} | head -n 1 # >> ${file}
+	return ${counter}
 }
 
-format "hello world"
-# echo " " # >> ${file}
-# echo "hello world" # >> ${file}
-# echo " " # >> ${file}
+function print_args() {
+	i=1
+	echo "args are: " 
+	for arg in $@; do
+	        echo "     (${i}) ${arg}"
+	        i=$((i + 1))
+	done
+}
 
-format ${HOME}
-# echo " " # >> ${file}
-# echo ${HOME} # >> ${file}
-# echo " " # >> ${file}
+#################### COMMANDS ####################
+
+echo "save" | format "hello world"
+counter=${PIPESTATUS[1]}
+
+echo "save" | format ${HOME}
+counter=${PIPESTATUS[1]}
 
 root=`ls /`
-format "this is root: `echo ${root}`"
-# echo " " # >> ${file}
-# echo "this is root: `echo ${root}`" # >> ${file}
-# echo " " # >> ${file}
+echo "save" | format "this is root: `echo ${root}`"
+counter=${PIPESTATUS[1]}
 
-format "`ls /home/joes/there-is-no-such-file`" || echo "--!error!--"
-# echo " " # >> ${file}
-# (echo "`ls /home/joes/there-is-no-such-file`" || echo "--!error!--") # >> ${file}
-# echo " " # >> ${file}
+# echo "save" | format "ls /home/joes/there-is-no-such-file" || echo "!error!"
+# counter=${PIPESTATUS[1]}
 
-format "`(ls /home/joes/there-is-no-such-file || echo "--!error!--")`"
-# echo " " # >> ${file}
-# (ls /home/joes/there-is-no-such-file || echo "--!error!--")
-# echo " " # >> ${file}
-
-# echo " " # >> ${file}
-function print_args() {
-        if [[ $# = 3 ]]; then
-                i=1
-                echo "args are: " # >> ${file}
-                for arg in $@; do
-                        echo "     (${i}) ${arg}" # >> ${file}
-                        i=$((i + 1))
-                done
-        else
-                echo "--!error!-- wrong number of arguments. call with" # >> ${file}
-                echo "     ./home/joes/bin/${0}  <foo> <bar> <baz>" # >> ${file}
-        fi
-}
+echo "save" | format "`(ls /home/joes/there-is-no-such-file 2>> /dev/null || echo "!error!")`"
+counter=${PIPESTATUS[1]}
 
 args=`print_args ${1} ${2} ${3}`
-# echo ${args}
-format "${args[@]}"
-# echo " " # >> ${file}
+echo "save" | format "${args[@]}"
+counter=${PIPESTATUS[1]}
+
+echo " "
+
+exit
