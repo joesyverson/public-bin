fa3a9b36d134236921dee8a052995f3c296fc18d
joes <josephgavinsyverson@gmail.com>
2019-12-31 00:19:16 -0500

    Add 05

---
+++ 05-ubuntu-setup.sh
@@ -0,0 +1,10 @@
+# the following is a script that sets up my personal machine as I like it
+# most of the packages are CLI tools
+
+# V1 will simply insall everything and list packages to be installed
+	# OPTIONS:
+		# -l, --list
+		# -h, --help
+# V2 will take arguments to remove packages from the default list
+	# OPTIONS
+		# -n, --not
